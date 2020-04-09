23e3c92c38e0b24c79e38dc4133178381c7fff3b
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-09 18:01:09 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -2,8 +2,9 @@ function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
-  console.log("so call me maybe");
+  console.log("so call me");
   return lyric;
+  var lyric = "maybe";
 }
 
 
